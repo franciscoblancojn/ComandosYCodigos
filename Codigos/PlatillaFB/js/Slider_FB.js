ec4c8a4deae43224303b5e6fda16192b2716ff4c
ters01 <blancofrancisco34@gmail.com>
2019-02-08 19:25:45 -0400

    agregando PlantillaFB

---
+++ Codigos/PlatillaFB/js/Slider_FB.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var arraySlider_FB = $(".Slider_FB");
+	for (var i = 0; i < arraySlider_FB.length; i++) {
+		$("#"+arraySlider_FB[i].id+" .content_FB");
+		//$(selector).html()
+		//$(selector).html(contenido)
+	}
+
+});
